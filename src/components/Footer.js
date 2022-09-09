9354ada71c6c6ea8646121abb8e03fa5bede2c66
Diego Cardoso <diegocar448@hotmail.com>
2022-09-09 14:23:12 -0300

    Post id

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
         <h3>Escreva sobre o que você têm interesse!</h3>
-        <p>Mini Blog &copy; 2022</p>
+        <p>Mini Blog &copy; {new Date().getFullYear()}</p>
     </footer>
   )
 }
